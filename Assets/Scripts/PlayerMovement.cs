6da72b83602273f19cdb144f10497aaa349b793b
Miku <mikulej7@gmail.com>
2026-04-27 10:45:05 +0200

    player camera input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,29 +2,41 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 10.0f;   
+    [SerializeField] float mouseSensetivity = 0.5f;
+    [SerializeField] float speed = 500.0f;   
     Vector3 velocity;
+    Camera playerCamera;
+    Vector3 cameraRotation;
+    Rigidbody rb;
 
     void Start()
-    {
+    { 
+        playerCamera = GetComponentInChildren<Camera>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        float step = speed * Time.deltaTime;
-
-        transform.position += velocity * step;
+        rb.linearVelocity = Quaternion.AngleAxis(cameraRotation.x, Vector3.up) * velocity * speed * Time.deltaTime;
     }
     public void OnJump()
     {
         Debug.Log("Jump!");
     }
 
-    public void OnMove(InputValue value)
+    public void OnMove(InputValue input)
     {
-        var v = value.Get<Vector2>();
-
+        var v = input.Get<Vector2>();
         velocity = new Vector3(v.x,0,v.y);
+    }
 
+    public void OnLook(InputValue input){
+        var v = input.Get<Vector2>() * mouseSensetivity;
+        cameraRotation.x += v.x;
+        cameraRotation.y += v.y;
+        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -90f,90f);
+
+        transform.localRotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+        playerCamera.transform.localRotation = Quaternion.AngleAxis(cameraRotation.y, Vector3.left);
     }
 }
