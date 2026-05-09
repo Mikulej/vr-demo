0acc819bdf874970cbb5c14a78098ef3aeccf2ce
Miku <mikulej7@gmail.com>
2026-05-09 12:02:18 +0200

    door sounds and locks

--- Assets/Scripts/FirstDoor.cs
+++ Assets/Scripts/FirstDoor.cs
@@ -3,18 +3,36 @@ using UnityEngine;
 public class FirstDoor : Interactable
 {
     public bool open = true;
+    [SerializeField] bool locked = false;
+    AudioSource soundEmitter;
+    [SerializeField] AudioClip lockedSound;
+    [SerializeField] AudioClip unlockSound;
+    [SerializeField] AudioClip interactSound;
     Animator anim;
     Animation a;
     void Start(){
         anim = GetComponent<Animator>();
+        soundEmitter = GetComponent<AudioSource>();
     }
     override public void Interact(){
-        if (open){
-            anim.SetTrigger("open");
+        if(locked){
+            soundEmitter.PlayOneShot(lockedSound);
         }
         else{
-            anim.SetTrigger("close");
+            if (open){
+            anim.SetTrigger("open");
+            }
+            else{
+                anim.SetTrigger("close");
+            }
+            soundEmitter.PlayOneShot(interactSound);
+            open = !open;
         }
-        open = !open;
+    }
+    public void Unlock(){
+        open = true;
+        locked = false;
+        anim.SetTrigger("open");
+        soundEmitter.PlayOneShot(unlockSound);
     }
 }
