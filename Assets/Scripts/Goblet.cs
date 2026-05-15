af70afe1eb6418dc7e5bed56b7bd78c1eea0b6a0
Miku <mikulej7@gmail.com>
2026-05-15 20:50:19 +0200

    third room layout

--- Assets/Scripts/Goblet.cs
+++ Assets/Scripts/Goblet.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class Goblet : MonoBehaviour
 {
     AudioSource soundEmitter;
     [SerializeField] AudioClip[] hitSounds = new AudioClip[5];
+    bool canPlaySound = false;
     void Start()
     {
         soundEmitter = GetComponent<AudioSource>();
+        StartCoroutine(enableSoundAfterDelay(1.0f));
     }
 
-    void OnCollisionEnter(){
-        int r = Random.Range(0,hitSounds.Length);
-        soundEmitter.PlayOneShot(hitSounds[r]);
-        Debug.Log("Play sound!");
+    void OnCollisionEnter(Collision other){
+        //&& other.gameObject.tag != "goblet" && other.gameObject.tag != "table"
+        if(canPlaySound ){
+            int r = Random.Range(0,hitSounds.Length);
+            soundEmitter.PlayOneShot(hitSounds[r]);
+            Debug.Log(other.gameObject.tag);
+        }
     }
     void OnCollisionExit(){
 
+    }
+
+    private IEnumerator enableSoundAfterDelay(float waitTime){
+        yield return new WaitForSeconds(waitTime);
+        canPlaySound = true;
     }
 }
